166fe7c7a1b4856921817997f3f45cc0d78ce12b
Chester Sim <ChesterSim@Chesters-MacBook-Pro.local>
2018-08-13 14:08:21 +0800

    lecture 3

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Chester Sim on 10/8/18.
+//  Copyright © 2018 Chester Sim. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
