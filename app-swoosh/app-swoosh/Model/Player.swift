b2138274d078461ce42a42f06b3f51ea248930ba
Chester Sim <ChesterSim@Chesters-MacBook-Pro.local>
2018-08-17 22:57:43 +0800

    final

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Chester Sim on 17/8/18.
+//  Copyright © 2018 Chester Sim. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
